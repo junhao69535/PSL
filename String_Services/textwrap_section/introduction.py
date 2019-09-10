70fe067f37a05360ced5ea6b23840e67e4b95175
Zhao Haobin <zhaohaobin@qianxin.com>
2019-09-10 15:11:27 +0800

    add content into textwrap introduction

---
+++ String_Services/textwrap_section/introduction.py
@@ -0,0 +1,13 @@
+#!coding=utf-8
+
+"""
+    源码：Lib/textwrap.py
+    这个模块提供了两个便捷的函数wrap()和fill()和一个能做所有工作的类TextWrapper，以及一个工具
+    函数dedeent()。如果只想包装或者填充一个或两个文本字符串，那两个便捷函数就足够用了，否则，
+    需要使用TextWrapper的实例更有效。
+"""
+
+# textwrap.wrap(text[,width[,...]])
+"""
+    
+"""
