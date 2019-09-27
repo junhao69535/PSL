0a19eab3a8afe2065404e5bf14a0c68b9114fe27
Zhao Haobin <zhaohaobin@qianxin.com>
2019-09-27 11:48:41 +0800

    add content

---
+++ Data_Types/datetime_section/available_types.py
@@ -0,0 +1,13 @@
+#!coding=utf-8
+import datetime
+"""
+    如题
+"""
+
+# class.datetime.date
+"""
+    一个理想化的naive日期，假设当前的公历始终是，并且始终是有效的。属性：year,month,day
+"""
+print datetime.date.year
+print datetime.date.month
+print datetime.date.day
